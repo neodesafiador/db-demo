1fc5309a3a53276c82aac30d4be1a8b69f56aae5
Katsuki Oike <katsuki.oike@gmail.com>
2023-02-28 19:42:27 -0600

    Create db-demo files

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import argon2 from 'argon2';
+import { addUser, getUserByEmail } from '../models/UserModel';
+import { parseDatabaseError } from '../utils/db-utils';
+
+async function registerUser(req: Request, res: Response): Promise<void> {
+  const { email, password } = req.body as AuthRequest;
+
+  // IMPORTANT: Hash the password
+  const passwordHash = await argon2.hash(password);
+
+  try {
+    // IMPORTANT: Store the `passwordHash` and NOT the plaintext password
+    const newUser = await addUser(email, passwordHash);
+    console.log(newUser);
+    res.sendStatus(201);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+async function logIn(req: Request, res: Response): Promise<void> {
+  const { email, password } = req.body as AuthRequest;
+
+  const user = await getUserByEmail(email);
+
+  // Check if the user account exists for that email
+  if (!user) {
+    res.sendStatus(404); // 404 Not Found (403 Forbidden would also make a lot of sense here)
+    return;
+  }
+
+  // The account exists so now we can check their password
+  const { passwordHash } = user;
+
+  // If the password does not match
+  if (!(await argon2.verify(passwordHash, password))) {
+    res.sendStatus(404); // 404 Not Found (403 Forbidden would also make a lot of sense here)
+    return;
+  }
+
+  // The user has successfully logged in
+  // NOTES: We will update this once we implement session management
+  res.sendStatus(200); // 200 OK
+}
+
+export { registerUser, logIn };
---
+++ src/types/utils.d.ts
@@ -0,0 +1,10 @@
+type DatabaseConstraintError = {
+  type: 'unique' | 'check' | 'not null' | 'foreign key' | 'unknown';
+  columnName?: string;
+  message?: string;
+};
+
+type AuthRequest = {
+  email: string;
+  password: string;
+};
