4f1e3bcd44ec75956afc1db3d367bbc984f8f9f8
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-03 16:09:08 -0500

    Add sendEmail() function

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,6 +11,7 @@ import {
   updateEmailAddress,
 } from '../models/UserModel';
 import { parseDatabaseError } from '../utils/db-utils';
+import { sendEmail } from '../services/emailServices';
 
 async function getAllUserProfiles(req: Request, res: Response): Promise<void> {
   res.json(await allUserData());
@@ -19,13 +20,14 @@ async function getAllUserProfiles(req: Request, res: Response): Promise<void> {
 async function registerUser(req: Request, res: Response): Promise<void> {
   const { firstName, lastName, email, password } = req.body as AuthRequest;
 
-  // IMPORTANT: Hash the password
+  // Hash the password
   const passwordHash = await argon2.hash(password);
 
   try {
-    // IMPORTANT: Store the `passwordHash` and NOT the plaintext password
+    // Store the `passwordHash` and NOT the plaintext password
     const newUser = await addUser(firstName, lastName, email, passwordHash);
     console.log(newUser);
+    await sendEmail(email, 'Welcome!', `Thank you for joining my application!`);
     res.sendStatus(201);
   } catch (err) {
     console.error(err);
@@ -38,15 +40,15 @@ async function logIn(req: Request, res: Response): Promise<void> {
   console.log(req.session);
 
   const now = new Date();
-  // NOTES: We need to convert the date string back into a Date() object
+  // We need to convert the date string back into a Date() object
   //        `parseISO()` does the conversion
   const logInTimeout = parseISO(req.session.logInTimeout);
-  // NOTES: If the client has a timeout set and it has not expired
+  // If the client has a timeout set and it has not expired
   if (logInTimeout && isBefore(now, logInTimeout)) {
-    // NOTES: This will create a human friendly duration message
+    // This will create a human friendly duration message
     const timeRemaining = formatDistanceToNow(logInTimeout);
     const message = `You have ${timeRemaining} remaining.`;
-    // NOTES: Reject their request
+    // Reject their request
     res.status(429).send(message); // 429 Too Many Requests
     return;
   }
@@ -61,29 +63,28 @@ async function logIn(req: Request, res: Response): Promise<void> {
 
   const { passwordHash } = user;
   if (!(await argon2.verify(passwordHash, password))) {
-    // NOTES: If they haven't attempted to log in yet
+    // If they haven't attempted to log in yet
     if (!req.session.logInAttempts) {
-      req.session.logInAttempts = 1; // NOTES: Set their attempts to one
+      req.session.logInAttempts = 1; // Set their attempts to one
     } else {
-      req.session.logInAttempts += 1; // NOTES: Otherwise increment their attempts
+      req.session.logInAttempts += 1; // Otherwise increment their attempts
     }
 
-    // NOTES: If the client has failed five times then we will add a
-    //        3 minute timeout
+    // If the client has failed five times then we will add a 3 minute timeout
     if (req.session.logInAttempts >= 5) {
-      const threeMinutesLater = addMinutes(now, 3).toISOString(); // NOTES: Must convert to a string
+      const threeMinutesLater = addMinutes(now, 3).toISOString(); // Must convert to a string
       req.session.logInTimeout = threeMinutesLater;
-      req.session.logInAttempts = 0; // NOTES: Reset their attempts
+      req.session.logInAttempts = 0; // Reset their attempts
     }
 
     res.sendStatus(404); // 404 Not Found - user with email/pass doesn't exist
     return;
   }
 
-  // NOTES: Remember to clear the session before setting their authenticated session data
+  // Remember to clear the session before setting their authenticated session data
   await req.session.clearSession();
 
-  // NOTES: Now we can add whatever data we want to the session
+  // Now we can add whatever data we want to the session
   req.session.authenticatedUser = {
     userId: user.userId,
     email: user.email,
@@ -118,18 +119,17 @@ async function resetProfileViews(req: Request, res: Response): Promise<void> {
 async function updateUserEmail(req: Request, res: Response): Promise<void> {
   const { targetUserId } = req.params as UserIdParam;
 
-  // NOTES: Access the data from `req.session`
+  // Access the data from `req.session`
   const { isLoggedIn, authenticatedUser } = req.session;
 
-  // NOTES: We need to make sure that this client is logged in AND
-  //        they are try to modify their own user account
+  // We need to make sure that this client is logged in AND
+  // they are try to modify their own user account
   if (!isLoggedIn || authenticatedUser.userId !== targetUserId) {
     res.sendStatus(403); // 403 Forbidden
     return;
   }
 
   const { email } = req.body as { email: string };
-
   // Get the user account
   const user = await getUserById(targetUserId);
 
