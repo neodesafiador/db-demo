2fd3f6df75130a614bf98c42b5b19a8373d5e04a
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-03 16:43:09 -0500

    Implement getBooks() function

--- src/models/BookModel.ts
+++ src/models/BookModel.ts
@@ -1,21 +1,29 @@
-import { AppDataSource } from "../dataSource";
-import { Book } from "../entities/Book";
+import { AppDataSource } from '../dataSource';
+import { Book } from '../entities/Book';
 
 const bookRepository = AppDataSource.getRepository(Book);
 
-async function addBook(title: string, publishedYear: number | undefined): Promise<Book> {
-    let newBook = new Book();
-    newBook.title = title;
-    newBook.publishedYear = publishedYear;
+async function addBook(title: string, publicationYear: number | undefined): Promise<Book> {
+  // Create the new Book object
+  let newBook = new Book();
+  newBook.title = title;
+  newBook.publishedYear = publicationYear;
 
-    newBook = await bookRepository.save(newBook);
+  newBook = await bookRepository.save(newBook);
 
-    return newBook;
+  return newBook;
 }
 
 async function getBookById(bookId: string): Promise<Book | null> {
-    return await bookRepository.findOne({ where: { bookId }});
+  return await bookRepository
+    .createQueryBuilder('book')
+    .leftJoinAndSelect('book.reviews', 'reviews')
+    .where('bookId = :bookId', { bookId })
+    .getOne();
 }
 
+async function getBooks(): Promise<Book[]> {
+  return await bookRepository.find();
+}
 
-export { addBook, getBookById };
+export { addBook, getBookById, getBooks };
