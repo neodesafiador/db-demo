fe29adafe147b60c496e30dfe7051d67b3fdf778
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-03 16:18:41 -0500

    Implement getBook and getAllBooks function

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -1,19 +1,37 @@
+import { Request, Response } from 'express';
+import { addBook, getBookById, getBooks } from '../models/BookModel';
 
+async function insertBook(req: Request, res: Response): Promise<void> {
+  const { isLoggedIn } = req.session;
+  if (!isLoggedIn) {
+    res.sendStatus(401);
+    return;
+  }
+  const { title, publicationYear } = req.body as NewBookRequest;
 
+  const book = await addBook(title, publicationYear);
+  console.log(book);
 
-type NewBookRequest
-
-async function addNewBook(req: Request, res: Response): Promise<void> {
-    if (!req.session.isLoggedIn) {
-        req.sendStatus(401);
-        return;
-    }
-
-    const { title, publishedYear } = rew.body as NewBookRequest;
-
-    try {
-        const newBook = await addNewBook(title, publishedYear);
-        console.log(newBook);
-
-    }
+  res.status(201).json(book);
 }
+
+async function getBook(req: Request, res: Response): Promise<void> {
+  const { bookId } = req.params as { bookId: string };
+
+  const book = await getBookById(bookId);
+
+  if (!book) {
+    res.sendStatus(404);
+    return;
+  }
+
+  console.log(book);
+
+  res.status(200).json(book);
+}
+
+async function getAllBooks(req: Request, res: Response): Promise<void> {
+  res.status(200).json(await getBooks());
+}
+
+export { insertBook, getBook, getAllBooks };
