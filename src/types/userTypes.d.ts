fd2af774c7d0bee6f10dff0dc88c25ce62ec358f
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-10 18:13:23 -0500

    Modify the type of User

--- src/types/userTypes.d.ts
+++ src/types/userTypes.d.ts
@@ -1,10 +1,8 @@
 type AuthRequest = {
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-  };
-  
-  type UserIdParam = {
-    targetUserId: string;
-  };
+  email: string;
+  password: string;
+};
+
+type UserIdParam = {
+  targetUserId: string;
+};
