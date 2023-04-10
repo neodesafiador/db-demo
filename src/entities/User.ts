63190729a7edf242498f0867976feada62b5fcc2
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-10 17:58:20 -0500

    Modify the relationship with book

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,20 +1,19 @@
 import {
-  Entity, PrimaryGeneratedColumn, Column, OneToOne, 
-  JoinColumn, Relation, OneToMany,
-} from "typeorm"
-import { AvatarPhoto } from './AvatarPhoto';
+  Entity,
+  PrimaryGeneratedColumn,
+  JoinTable,
+  ManyToMany,
+  Column,
+  OneToMany,
+  Relation,
+} from 'typeorm';
 import { Review } from './Review';
+import { Book } from './Book';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   userId: string;
-
-  @Column()
-  firstName: string;
-
-  @Column()
-  lastName: string;
 
   @Column({ unique: true })
   email: string;
@@ -28,10 +27,10 @@ export class User {
   @Column({ default: 0 })
   profileViews: number;
 
-  @OneToOne(() => AvatarPhoto, (avatarPhoto) => avatarPhoto.user, { cascade: ['insert', 'update'] })
-  @JoinColumn()
-  avatarPhoto: Relation<AvatarPhoto>;
-
   @OneToMany(() => Review, (review) => review.user, { cascade: ['insert', 'update'] })
   reviews: Relation<Review>[];
+
+  @ManyToMany(() => Book, (book) => book.users, { cascade: ['insert', 'update'] })
+  @JoinTable()
+  books: Relation<Book>[];
 }
