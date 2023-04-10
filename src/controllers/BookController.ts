8551119eee9d31ea67a1796c96402a9c23b806de
Katsuki Oike <katsuki.oike@gmail.com>
2023-04-10 10:51:33 -0500

    Render it with EJS

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -4,13 +4,17 @@ import { addBook, getBookById, getBooks } from '../models/BookModel';
 async function insertBook(req: Request, res: Response): Promise<void> {
   const { isLoggedIn } = req.session;
   if (!isLoggedIn) {
-    res.sendStatus(401);
+    // res.sendStatus(401);
+    res.redirect('/login');
     return;
   }
-  const { title, publicationYear } = req.body as NewBookRequest;
+  const { title, publicationYear, inPublicDomain } = req.body as NewBookRequest;
+  console.log(`inPublicDomain: ${inPublicDomain}`);
+  console.log(`inPublicDomain after converting: ${!!inPublicDomain}`);
 
-  const book = await addBook(title, publicationYear);
-  console.log(book);
+
+  const book = await addBook(title, publicationYear, !!inPublicDomain);
+  // console.log(book);
 
   res.status(201).json(book);
 }
@@ -25,13 +29,17 @@ async function getBook(req: Request, res: Response): Promise<void> {
     return;
   }
 
-  console.log(book);
-
-  res.status(200).json(book);
+  // res.status(200).json(book);
+  res.render('bookPage', { book });
 }
 
 async function getAllBooks(req: Request, res: Response): Promise<void> {
-  res.status(200).json(await getBooks());
+  // Don't send back the raw data. Instead render it with EJS
+  // res.status(200).json(await getBooks());
+
+  const books = await getBooks();
+
+  res.render('libraryPage', { books });
 }
 
 export { insertBook, getBook, getAllBooks };
